73fdcef2b420a7fef7860e559ae89519cf77ed79
Ishimwe William <ishimwe.william2000@gmail.com>
2025-01-02 14:14:08 +0200

    added dht11 folder

---
+++ dht11/README.md
@@ -0,0 +1,7 @@
+# faranux-dht11-test
+
+```bash
+allow="clipboard-read; clipboard-write"
+scrolling="yes"
+style="... height:500px;"
+```
---
+++ dht11/faranux-dht11-test/faranux-dht11-test.ino
@@ -0,0 +1,36 @@
+#include "DHT.h"
+
+#define DHTPIN 2       // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11  // DHT 11
+
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHT11 test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);  // Wait a few seconds between measurements.
+
+  float h = dht.readHumidity();         // Read humidity in Percentage
+  float t = dht.readTemperature();      // Read temperature as Celsius (the default)
+  float f = dht.readTemperature(true);  // Read temperature as Fahrenheit (isFahrenheit = true)
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.println(F("°F"));
+}
